29781e595a6aa2a9d5e0ab2169b74b026de6b452
Genesis Carrero <85486899+genesisscv@users.noreply.github.com>
2024-10-14 09:36:07 +1000

    First commit

---
+++ shopping-site/shopping-site-web-parent/shopping-site-front-end/src/main/java/com/example/shoppingsitefrontend/ShoppingSiteFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.shoppingsitefrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingSiteFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingSiteFrontEndApplication.class, args);
+	}
+
+}
---
+++ shopping-site/shopping-site-web-parent/shopping-site-front-end/src/test/java/com/example/shoppingsitefrontend/ShoppingSiteFrontEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoppingsitefrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingSiteFrontEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
